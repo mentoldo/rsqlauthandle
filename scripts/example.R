f90b32897fa8c2599bf393af9db856ee2d1f4e1c
mentoldo <matias.alfonso@gmail.com>
2021-12-10 20:33:14 -0300

    Se actualizó ejemplo

--- scripts/example.R
+++ scripts/example.R
@@ -4,7 +4,7 @@ auth$set_credentials(dialect = 'postgresql',
                      port = '5432',
                      user = 'matias',
                      db_name = 'conectividad',
-                     passwd = 'r0Merodelacasa')
+                     passwd = '1234')
 
 kb <- keyring::backend_file$new()
 kb$keyring_create()
@@ -14,3 +14,10 @@ print(auth$config)
 library(DBI)
 con <- connect_postgres(alias='rsqlauthandle')
 dbListTables(con)
+
+set_credentials('matias',
+                'conectividad')
+con <- connect_postgres(alias='rsqlauthandle')
+dbListTables(con)
+dbReadTable(con, 't_respuestas')
+dbReadTable(con, 't_respuestas')
